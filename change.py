f8f757732ede3b4e3aaa1002a521f25f0817b0a3
purpl3-f0x <42351007+purpl3-f0x@users.noreply.github.com>
2019-02-25 20:03:59 -0500

    Update change.py

--- change.py
+++ change.py
@@ -4,10 +4,13 @@ import os
 import random
 import string
 
-
+# Creates a variable named password and assigns it a string of 32 random characters.
 password = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
 
+# Creates a variable named user and assigns it the value of the currently logged on user.
 user = getpass.getuser()
+
+# Calls sudo, then chpassword, and then changes the currently logged on user's password.
 p = subprocess.Popen(["sudo", "chpasswd"],
     shell=False,
     stdin=subprocess.PIPE,
@@ -15,4 +18,5 @@ p = subprocess.Popen(["sudo", "chpasswd"],
     stderr=subprocess.PIPE)
 p.communicate(user+":"+password)
 
+# Loggs the user off.
 os.system("gnome-session-quit --force")
